90be7710383cb24fdb3610f07ae8ad29758a4a37
EUTyrin <e_tyrin@eureca.ru>
2020-08-27 10:56:37 +0300

    many files

--- src/main/java/evgenyt/testfilesgenerator/TestFilesGenerator.java
+++ src/main/java/evgenyt/testfilesgenerator/TestFilesGenerator.java
@@ -4,26 +4,50 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 import java.util.Scanner;
 
+/**
+ * Text files generator
+ */
+
 public class TestFilesGenerator {
+
+    private static final SimpleDateFormat dateFormat =
+            new SimpleDateFormat("ddMMyy-hhmmssSSS");
+    private static final Random random = new Random();
+    private static String[] words;
+
     public static void main(String[] args) throws FileNotFoundException {
+        // Build dictionary
         Scanner scanner = new Scanner(new File(System.getProperty("user.dir") + "/words.txt"));
         String wordsStr = scanner.useDelimiter("\\A").next();
         scanner.close();
-        String[] words = wordsStr.split(" ");
+        words = wordsStr.split(" ");
         System.out.println("Words loaded: " + words.length);
-        StringBuilder newStr = new StringBuilder();
-        Random random = new Random();
+        // Ask user for size and count of files
         Scanner inputScanner = new Scanner(System.in);
-        System.out.println("Input words num");
-        int targetLength = inputScanner.nextInt();
-        int chunkLength = 10000000;
+        System.out.print("Input file size in Mb: ");
+        int fileSize = inputScanner.nextInt();
+        System.out.print("Input number of files: ");
+        int fileCount = inputScanner.nextInt();
+        // Generate files
+        for (int count = 1; count <=fileCount; count++) {
+            generateFile(fileSize);
+        }
+    }
+
+    // Generate text file near of specified size in Mb
+    public static void generateFile(int fileSize) throws FileNotFoundException {
+        int targetLength = fileSize * 1000000 / 8; // Size in words
+        int chunkLength = 10000000; // Size of write portion
         int chunks = targetLength / chunkLength;
         int modulo = targetLength - (chunkLength * chunks);
-        PrintWriter printWriter = new PrintWriter(new FileOutputStream(
-                new File("test.txt")));
+        String fileName = "test-" + String.format("%s", dateFormat.format(new Date())) + ".txt";
+        System.out.println("Writing to:" + fileName);
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream(new File(fileName)));
         for (int chunkIdx = 1; chunkIdx <= chunks; chunkIdx++) {
             System.out.println("Chunk: " + chunkIdx);
             StringBuilder chunkStr = new StringBuilder();
@@ -41,4 +65,5 @@ public class TestFilesGenerator {
         printWriter.append(moduloStr);
         printWriter.close();
     }
+
 }
